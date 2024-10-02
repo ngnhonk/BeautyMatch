6251a2ee0bfca3d6c9104f7802e0b86a97f1a056
Honk <hungnguyenviet204@gmail.com>
2024-10-02 22:25:55 +0700

    fix footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,14 @@ window.addEventListener('scroll', function() {
 
 document.getElementById('menu').addEventListener('click', function () {
     var hMenu = document.getElementById('hMenu');
+    event.stopPropagation();
     if (hMenu.style.display === 'block') {
         hMenu.style.display = 'none';
+        var menuDiv = document.getElementById('menu');
+        menuDiv.innerHTML = '<i class="fa-solid fa-bars"></i>';
     } else {
         hMenu.style.display = 'block';
+        var menuDiv = document.getElementById('menu');
+        menuDiv.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     }
 });
