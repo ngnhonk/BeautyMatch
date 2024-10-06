c70737fd3b5fd2149d5787cf47bca30fae8362d0
Honk <hungnguyenviet204@gmail.com>
2024-10-06 16:05:43 +0700

    dunno

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) { // Điều kiện cuộn xuống 50px
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) { // Điều kiện cuộn xuống 50px
+      header.classList.add('scrolled');
+  } else {
+      header.classList.remove('scrolled');
+  }
 });
 
 document.getElementById('menu').addEventListener('click', function () {
-    var hMenu = document.getElementById('hMenu');
-    event.stopPropagation();
-    if (hMenu.style.display === 'block') {
-        hMenu.style.display = 'none';
-        var menuDiv = document.getElementById('menu');
-        menuDiv.innerHTML = '<i class="fa-solid fa-bars"></i>';
-    } else {
-        hMenu.style.display = 'block';
-        var menuDiv = document.getElementById('menu');
-        menuDiv.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-    }
+  var hMenu = document.getElementById('hMenu');
+  event.stopPropagation();
+  if (hMenu.style.display === 'block') {
+      hMenu.style.display = 'none';
+      var menuDiv = document.getElementById('menu');
+      menuDiv.innerHTML = '<i class="fa-solid fa-bars"></i>';
+  } else {
+      hMenu.style.display = 'block';
+      var menuDiv = document.getElementById('menu');
+      menuDiv.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  }
 });
